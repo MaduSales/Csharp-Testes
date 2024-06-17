176dc44f6013ee8a983d438f181f86cf757b31f5
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-16 21:38:22 -0300

    Add files via upload

---
+++ operadoresAtribuicao07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exemplo07{
+
+    class OperadorAtribuicaoBinario{
+        public static void Main(string[] args){
+            int num = 6; //0110 em binário
+            num &= 3; // 0011 em binário
+            // o operador &= faz a operação bitwise AND (bit a bit "E"):
+
+            //0110
+            //0011
+            //-----
+            //0010 (é o número 2 representado em binário)
+            Console.WriteLine(num); // deve retornar 2
+        }
+    }
+}
