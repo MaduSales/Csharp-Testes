2e75e2e965f446030da98b4f5afb0b6388214c15
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-19 23:09:17 -0300

    Add files via upload

---
+++ operadoresLogicos01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo01{
+
+    class OperadorLogicoE{
+        public static void Main(string[] args){
+            int x = 10;
+
+            Console.Write(x < 200 && x > 1);
+        }
+    }
+}
