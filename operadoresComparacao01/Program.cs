6e2de80f4d4002953126d77d83a451e8b59fb520
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-21 22:09:52 -0300

    Add files via upload

---
+++ operadoresComparacao01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exemplo01{
+
+    class OperadorComparacao{
+        public static void Main(string[] args){
+            int x = 5;
+            int y = 4;
+
+            Console.Write(x == y); // deve retornar False
+        }
+    }
+}
