a1b0b62de8eb54a5090fd06a0ce813d597cd7caa
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-06-25 22:37:58 -0300

    Add files via upload

---
+++ operadoresAtribuicao08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exemplo08{
+
+    class OperadorAtribuicaoBinarioOu{
+        public static void Main(string[] args){
+
+        int num = 5; //0101
+        num |= 3; //0011
+        //O operador |= é um operador de atribuição bit a bit em Python. Ele realiza uma operação "OU" (OR) bit a bit entre a variável à esquerda e o valor à direita, e depois atribui o resultado de volta à variável à esquerda.
+
+        //0101 (05)
+        //0011 (03)
+        //-----
+        //0111 (número 07 em binário)
+        Console.WriteLine(num);
+        }
+    }
+}
